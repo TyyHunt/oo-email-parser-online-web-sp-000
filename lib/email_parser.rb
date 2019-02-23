b7735866a2abce7b1712e151d06c05f029eb9b2e
Tyler Hunt <tyhunt04@gmail.com>
2019-02-23 22:28:04 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailParser
   def parse
     email_array = []
     email_array << @emails.split(/[, ]/)
-    email_array.reject {|email| email.empty?}
+    email_array.reject! {|email| email.empty?}
   end
 
 end
